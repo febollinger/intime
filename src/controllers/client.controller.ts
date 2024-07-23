0e7b3fd322406442a3cb6e41fc6b5ca3ab947477
Fernanda Bollinger <feernanda.bollinger@gmail.com>
2024-07-23 09:21:50 -0300

    firstController

---
+++ src/controllers/client.controller.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express"
+
+export const createClientController = async (request: Request, response: Response): Promise<Response> => {
+    const clientReq = request.body
+    
+
+    return response
+
+}
